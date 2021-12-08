00a78b69c2b609f41fb1853c0a1e80fa5ac0c00a
Angelo <angelo.lamonaca@gmail.com>
2021-12-09 00:47:20 +0100

    💫 Add animations and translations

--- src/app/controllers/EventController.ts
+++ src/app/controllers/EventController.ts
@@ -3,12 +3,14 @@ import { CameraController } from './CameraController';
 import { fromEvent, Observable, tap } from 'rxjs';
 
 export class EventController {
+    previosWindowInnerWidth: number;
     resizeEvents$: Observable<Event> = fromEvent(window, 'resize').pipe(
         tap(() => this.onWindowResize()),
         tap((event) => console.log(event))
     );
 
     constructor() {
+        this.previosWindowInnerWidth = window.innerWidth;
         this.resizeEvents$.subscribe();
         window.onbeforeunload = () => {
             window.scrollTo(0, 0);
@@ -16,9 +18,12 @@ export class EventController {
     }
 
     onWindowResize(): void {
-        CameraController.Instance.camera.aspect = window.innerWidth / window.innerHeight;
-        CameraController.Instance.camera.updateProjectionMatrix();
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        render();
+        if (window.innerWidth != this.previosWindowInnerWidth) {
+            this.previosWindowInnerWidth = window.innerWidth;
+            CameraController.Instance.camera.aspect = window.innerWidth / window.innerHeight;
+            CameraController.Instance.camera.updateProjectionMatrix();
+            renderer.setSize(window.innerWidth, window.innerHeight);
+            render();
+        }
     }
 }
--- src/app/controllers/LoadingController.ts
+++ src/app/controllers/LoadingController.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { animate } from '../app';
+import { forEach } from 'lodash-es';
 
 export class LoadingController {
     private static _instance: LoadingController;
@@ -40,11 +41,36 @@ class LoadingManager extends THREE.LoadingManager {
                 ' files.'
         );
     };
-    onLoad = (): void => {
+    onLoad = async (): Promise<void> => {
         console.log('Loading complete!');
         animate();
+        await delay(3000);
+        document.getElementById('loading')!.style.opacity = '0';
+        document.getElementById('loading')!.style.zIndex = '0';
+        await delay(1000);
+        document.getElementsByTagName('canvas')![0].style.opacity = '1';
+        await delay(1000);
+        document.getElementById('container')!.style.opacity = '1';
+        const list = document.getElementsByClassName('button');
+        for (let item of list) {
+            item.animate(
+                [
+                    // keyframes
+                    { transform: 'translateX(-300px)' },
+                    { transition: 'transform 1s default' },
+                ],
+                {
+                    // timing options
+                    duration: 2000,
+                }
+            );
+        }
     };
     onError = (url: string): void => {
         console.log('There was an error loadindg ' + url);
     };
+}
+
+function delay(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
